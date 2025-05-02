476475776290b392161a9ffcdb5e1c9efa6155b1
Willi-2512 <williamestebanchacon@gmail.com>
2025-05-02 02:23:05 -0500

    se agrega la estructura y volcado de datos para la tabla `solicitudes`, se establecen índices y restricciones, y se actualiza el AUTO_INCREMENT para las tablas afectadas

--- RenaceHogares/proyecto2025h.sql
+++ RenaceHogares/proyecto2025h.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-05-2025 a las 07:39:20
+-- Tiempo de generación: 02-05-2025 a las 09:21:28
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `proyecto2025h`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitudes`
+--
+
+CREATE TABLE `solicitudes` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `tipo_solicitud` varchar(100) NOT NULL,
+  `descripcion` text NOT NULL,
+  `fecha_solicitud` timestamp NOT NULL DEFAULT current_timestamp(),
+  `estado` varchar(30) NOT NULL DEFAULT 'En espera'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `solicitudes`
+--
+
+INSERT INTO `solicitudes` (`id`, `usuario_id`, `tipo_solicitud`, `descripcion`, `fecha_solicitud`, `estado`) VALUES
+(1, 8, 'Reparación de vivienda', 'ayuda me paso esto ,esto y esto', '2025-05-02 06:47:47', 'En proceso'),
+(2, 8, 'Reparación de vivienda', 'ayuda me paso esto ,esto y esto', '2025-05-02 06:49:05', 'En espera'),
+(3, 9, 'Reparación de vivienda', 'me paso esto aquello y lo otro', '2025-05-02 07:00:15', 'En espera');
 
 -- --------------------------------------------------------
 
@@ -43,12 +67,19 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `cedula`, `telefono`, `email`, `direccion`, `password`, `fecha_registro`) VALUES
-(5, 'willi', '1234243', '3224512423', 'a@a.com', 'calle 45 b norte', '$2y$10$cJRUxsXCZxpBmRD84zntQ.wWgeFCEDhS0ufXjG8cR8r9x1pd5DOwu', '2025-04-27 22:29:46'),
-(6, 'prueba', '12323432342', '4433423443', 'prueba@a.com', 'Calle633sur#93a90', '$2y$10$zaAySfUnbIR2TfXt.Nnj1OJb6Nol6QN2srqOa.S/ZNwl.WAo87/g2', '2025-04-27 22:45:41');
+(8, 'prueba', '12323432342', '4433423443', 'prueba@a.com', 'Calle633sur#93a90', '$2y$10$RUn5u8GbW0Jf5r7H16sCVOHm.x8sVCZRC93uCXGJzcFIzag1Gb9sK', '2025-05-02 05:48:46'),
+(9, 'prueba', '1055779955', '3003211225', 'datos1@a.com', 'calle 25 a sur', '$2y$10$.JsuZTUguSeTPoS/WigTVubOQOFfYSo7PXpRj7UgW/ESVGgfRWy3m', '2025-05-02 06:59:31');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `solicitudes`
+--
+ALTER TABLE `solicitudes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -62,10 +93,26 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `solicitudes`
+--
+ALTER TABLE `solicitudes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `solicitudes`
+--
+ALTER TABLE `solicitudes`
+  ADD CONSTRAINT `solicitudes_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
